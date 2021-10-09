311e77a7fe5ef61f434840f4b49bf203372308c8
Diego Guerra <diegoguerradiaz@gmail.com>
2021-10-09 14:41:31 -0300

    created code to create a new task

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # READ all tasks
   get 'tasks', to: 'tasks#index'
+  # CREATE new task
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
   # READ one task
   get 'tasks/:id', to: 'tasks#show', as: :task
 end
