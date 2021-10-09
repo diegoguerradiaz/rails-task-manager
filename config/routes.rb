e1cd62134363d8928d0768aedbe1ab7f66273de7
Diego Guerra <diegoguerradiaz@gmail.com>
2021-10-09 12:50:39 -0300

    added code to display specific tasks

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # READ all tasks
   get 'tasks', to: 'tasks#index'
+  # READ one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
